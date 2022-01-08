7683d8a71852da04bab298a03aa1b386a5454fe2
lyx <lyxlyxi@hotmail.com>
2022-01-08 17:34:59 +0800

    update scripts

--- .scripts/heroku-push-web.sh
+++ .scripts/heroku-push-web.sh
@@ -5,6 +5,8 @@ BRANCH=`git rev-parse --abbrev-ref HEAD`
 echo -n "DEBUG_MODE (y/n): "
 read DEBUG
 
+# check new branch
+
 git checkout -b heroku_${BRANCH}
 git pull heroku main
 if [ `echo $DEBUG | grep -i y` ]; then 
@@ -14,18 +16,18 @@ echo "web: gunicorn app:server" > Procfile
 fi
 git add Procfile
 
-if [ -f runtime.txt ]; then
+if [ ! -f runtime.txt ]; then
 echo "python-3.8.12" > runtime.txt
 git add runtime.txt
 fi
 
-if [ -f requirements.txt ] ; then
+if [ ! -f requirements.txt ] ; then
 pip freeze > requirements.txt
 git add requirements.txt
 fi
 
 git commit -m "deploy"
-
+read x
 git branch -M main
 git push -u heroku main
 if [ $? -eq 0 ]; then
