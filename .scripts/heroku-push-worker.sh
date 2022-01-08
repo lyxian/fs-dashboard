e4406c13ce49586c6a762c4f0b4d2142f13fc1b0
lyx <lyxlyxi@hotmail.com>
2022-01-08 17:18:43 +0800

    update scripts

--- .scripts/heroku-push-worker.sh
+++ .scripts/heroku-push-worker.sh
@@ -2,20 +2,24 @@
 
 BRANCH=`git rev-parse --abbrev-ref HEAD`
 
-echo -n "DEBUG_MODE (y/n): "
-read DEBUG
-
 git checkout -b heroku_${BRANCH}
 git pull heroku main
-if [ `echo $DEBUG | grep -i y` ]; then 
-echo "web: python app.py" > Procfile
-else
-echo "web: gunicorn app:server" > Procfile
+
+if [ -f runtime.txt]; then
+echo "worker: python main.py" > Procfile
+git add Procfile
 fi
 
+if [ -f runtime.txt]; then
 echo "python-3.8.12" > runtime.txt
+git add runtime.txt
+fi
+
+if [-f requirements.txt]; then
 pip freeze > requirements.txt
-git add Procfile runtime.txt requirements.txt 
+git add requirements.txt
+fi
+
 git commit -m "deploy"
 
 git branch -M main
